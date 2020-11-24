3ae9c9af2349af00ae19c48737beb40cf37bc5c2
benjamin32561 <62675494+benjamin32561@users.noreply.github.com>
2020-11-24 18:20:14 +0200

    changed the gender-detection model and the code

--- gender_detection.py
+++ gender_detection.py
@@ -2,21 +2,16 @@ from tensorflow.keras import models
 import numpy as np
 
 class genderDetector():
-	def __init__(self, con_th = 0.5, model_path = "models/gender.h5"):
+	def __init__(self, model_path = "models/gender-detection.h5"):
 		self.model = models.load_model(model_path)
-		self.conf_th = con_th
 
-	def checkNonmaskers(self, face):
-		check = []
-		for i in range(0, len(faces)):
-			check.append(faces[i][2])
-		check = np.array(check)
-		predictions = self.model.predict(check)
+	def checkGenders(self, faces):
+		predictions = self.model.predict(faces)
 		toRet = []
-		for i in range(0, len(check)):
+		for i in range(0, len(predictions)):
 			print(predictions[i])
-			if predictions[i] > self.conf_th:
-				toRet.append(true)
+			if predictions[i] > 0.5:
+				toRet.append(True)
 			else:
-				toRet.append(false)
+				toRet.append(False)
 		return toRet
