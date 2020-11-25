44ad0eb8dfb26f5796f02f3364e90bc80380ac85
benjamin32561 <62675494+benjamin32561@users.noreply.github.com>
2020-11-25 10:48:51 +0200

    added comments to the gender-detection.py file

--- gender_detection.py
+++ gender_detection.py
@@ -3,11 +3,20 @@ import numpy as np
 
 class genderDetector():
 	def __init__(self, model_path = "models/gender-detection.h5"):
-		self.model = models.load_model(model_path)
+		"""
+		this is the C'tor of the class
+		input: model path
+		output: non
+		"""
+		self.model = models.load_model(model_path)#loading the model
 
 	def checkGenders(self, faces, faces_loc):
-		predictions = self.model.predict(faces)
-		for i in range(0, len(predictions)):
-			print(predictions[i])
-			faces_loc[i].append(predictions[i] < 0.5)
+		"""
+		this method checks the gender of given faces
+		input: an array of faces and an array of their locations
+		output: the location and gender of the faces
+		"""
+		predictions = self.model.predict(faces)#getting the predictions for all the faces
+		for i in range(0, len(predictions)):#going through the predictions
+			faces_loc[i].append(predictions[i] < 0.5)#adding the gender to the location of the face
 		return faces_loc
