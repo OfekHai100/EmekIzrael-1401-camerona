34a629f2da89a0ed4b50c157bbc3e632825f9318
benjamin32561 <62675494+benjamin32561@users.noreply.github.com>
2020-12-22 11:59:56 +0200

    chenged some files

--- training/dataset/transfer.py
+++ training/dataset/transfer.py
@@ -1,9 +1,21 @@
 import os
 
 def main():
-    path = "backup\\"
-    dst = "with_mask\\"
-    images = os.listdir(path)
+    path = "all_data\\"
+    dst = "dest\\"
+    folders = os.listdir(path)
+    i = 1
+    for folder in folders:
+    	print(folder)
+    	new_pth = os.path.join(path, folder)
+    	images = os.listdir(new_pth)
+    	for image in images:
+    		input_path = os.path.join(new_pth, image)
+    		output_path = os.path.join(dst, str(i)+'.jpg')
+    		os.rename(input_path, output_path)
+    		i += 1
+
+    """
     i = len(os.listdir(dst)) + 1
     break_at = int(input("how much photos to transfer? "))
     for image_path in images:
@@ -12,7 +24,7 @@ def main():
         input_path = os.path.join(path, image_path)
         output_path = os.path.join(dst, str(i)+'.jpg')
         os.rename(input_path, output_path)
-        i += 1
+        i += 1"""
 
 if __name__ == '__main__':
     main()
