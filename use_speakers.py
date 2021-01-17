5a4dc0b210fd47429c34e0f6886f29b006c22229
benjamin32561 <62675494+benjamin32561@users.noreply.github.com>
2021-01-17 19:30:02 +0200

    orginaized all files

--- use_speakers.py
+++
@@ -1,8 +0,0 @@
-import winsound
-import time
-
-def play(gender):
-	if gender:
-		winsound.PlaySound("voices/wear mask for male.wav", winsound.SND_FILENAME)
-	else:
-		winsound.PlaySound("voices/wear mask for male.wav", winsound.SND_FILENAME)
